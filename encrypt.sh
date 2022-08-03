833985ca28614f3cfdf8a48e077aab8cb964bf33
Mathieu Lemay <mlemay@digitalubiquitycapital.com>
2022-08-03 08:38:25 -0400

    Changes to Structure
    Signed-off-by: Mathieu Lemay <mlemay@digitalubiquitycapital.com>

--- encrypt.sh
+++ encrypt.sh
@@ -1,1 +1,1 @@
-find public -type f -name "*.html" -not -name "*_encrypted.html" -exec staticrypt {} test -o {} \;
+find public -type f -name "*.html" -not -name "*_encrypted.html" -exec staticrypt -f ../password_template.html {} valo2022 -o {} \;
